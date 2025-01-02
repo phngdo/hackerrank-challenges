202916c08f1646885fc29326c1479e078f36a6fd
Phuong Le Do <joni.phngdo@gmail.com>
2025-01-02 11:12:35 +0700

    17. Weather Observation Station 12

---
+++ SQL/SQL - Basic/17. Weather Observation Station 12.sql
@@ -0,0 +1,5 @@
+-- It works well with MS SQL Server and MySQL
+SELECT DISTINCT CITY
+FROM STATION
+WHERE UPPER(LEFT(CITY, 1)) NOT IN ('a', 'o', 'e', 'u', 'i')
+  AND LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'o', 'e', 'u', 'i');
