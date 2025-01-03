f317f953edb8b8b85b149b6e34828b331799770c
Phuong Le Do <joni.phngdo@gmail.com>
2025-01-03 14:14:45 +0700

    01. Student Advisor

---
+++ SQL/SQL - Exam Preparation/01. Student Advisor.sql
@@ -0,0 +1,8 @@
+-- MS SQL Server
+SELECT student.roll_number,
+       student.name
+FROM student_information AS student
+INNER JOIN faculty_information AS faculty
+        ON student.advisor = faculty.employee_ID
+WHERE (faculty.gender = 'M' AND faculty.salary > 15000)
+   OR (faculty.gender = 'F' AND faculty.salary > 20000);
